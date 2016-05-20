79955a15c81ca225b14dc82c12457dff1b824c8a
aporzier <alexis.porzier@capgemini.com>
2016-05-20 14:34:30 +0200

    Mise en place de l'héritage model + premières ligne du serveur websocket

--- src/server/game/index.js
+++ src/server/game/index.js
@@ -1,12 +1,20 @@
-var http = require('http');
+require("babel-register")({
+	extensions: [".js6"]
+});
+var Game = require('../metier/Game.js6');
+
+var g = new Game.default();
+
+/*
+ var http = require('http');
 var server = http.createServer(function(req, res){
 
 });
 
-var io = require('socket.io').listeni(server);
+var io = require('socket.io').listen(server);
 
-io.socket.on('connection'), function(socket){
+io.socket.on('connection', function(socket){
 	console.log('Un client s\'est connecté');
 });
 
-server.listen(8000);
+server.listen(8000);*/
